2a1a8fba89096beaac67f975f43fb8a04ddddfdc
Sean Aye <hello@seanaye.ca>
2022-12-05 17:47:36 -0500

    update logging

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 import { serve } from "https://deno.land/std/http/server.ts"
 
-const wasmUrl = new URL("/main.wasm", import.meta.url).href
+const wasmUrl = new URL("main.wasm", import.meta.url).href
 
 serve(async () => {
-  console.log({ wasmUrl })
+  console.log({ wasmUrl, url: import.meta.url })
   const resp = await fetch(wasmUrl)
   return new Response(resp.body, {
     headers: {
